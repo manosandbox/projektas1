eb74c141e3d29a6e346653880d832601acfb7f70
vardas <gaubysv@gmail.com>
2017-02-13 21:31:41 +0200

    initial project code commit

---
+++ src/appclasses/IPLocationFinder.java
@@ -0,0 +1,41 @@
+package appclasses;
+
+import java.rmi.RemoteException;
+
+import javax.xml.rpc.ServiceException;
+
+import org.apache.axis.AxisFault;
+
+import net.webservicex.www.GeoIP;
+import net.webservicex.www.GeoIPService;
+import net.webservicex.www.GeoIPServiceLocator;
+import net.webservicex.www.GeoIPServiceSoap;
+import net.webservicex.www.GeoIPServiceSoapStub;
+
+public class IPLocationFinder {
+
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.out.println("You need to pass in one IP adress");
+		}
+		else {
+			String ipAddress = args[0];
+			try {
+				GeoIPServiceLocator service = new GeoIPServiceLocator();
+				   
+				 GeoIPServiceSoap port = service.getGeoIPServiceSoap();
+				   
+				 GeoIP geoIp = port.getGeoIP(ipAddress);
+				 //System.out.println(geoIp.getCountryName());﻿
+			} catch (AxisFault e) {
+				e.printStackTrace();
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			} catch (ServiceException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
